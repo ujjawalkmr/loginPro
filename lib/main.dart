2435802d603549d866117a7d5cf1d35019c157d4
ujjawalkmr <ujjawalkmr9211@gmail.com>
2023-05-08 17:56:03 +0530

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,17 +16,22 @@ class FrantendLogin extends StatefulWidget {
 }
 
 class _FrantendLoginState extends State<FrantendLogin> {
+  //this variable is used to validate the form
+ final _formKey=GlobalKey<FormState>();
+bool validationEmail=false;
+bool validationPassword=false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // resizeToAvoidBottomInset: true,
+         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         body: SafeArea(
           child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  //this is for first heading line
                   Container(
                     margin: EdgeInsets.only(top: 70.0),
                   child: Stack(
@@ -40,6 +46,7 @@ class _FrantendLoginState extends State<FrantendLogin> {
                           color: const Color(0xff404A6F),
                         ),
                       ),
+                      //this is used to positioned the circleAvtar
                       Positioned(
                         top: -50,
                         child: Container(
@@ -59,6 +66,7 @@ class _FrantendLoginState extends State<FrantendLogin> {
                     ],
                   ),
                 ),
+                  //this is used to give the space between first container and 2nd
                   SizedBox(height: 30,),
 
 
@@ -66,8 +74,11 @@ class _FrantendLoginState extends State<FrantendLogin> {
                     //This stack is used to give form validation background plate
                     child: Stack(
                       children: [
+                        //this is use to the give margin
                         Container(
-                          constraints: BoxConstraints.expand(width: 350.0,height: 400.0),
+                          constraints: BoxConstraints.expand(width: 350.0,height: 350.0),
+                          height: 350,width: 350,
+                          //
                           child: Card(
                             shadowColor: const Color(0xff404A6F),
                             elevation: 5,
@@ -94,68 +105,229 @@ class _FrantendLoginState extends State<FrantendLogin> {
                         //Form Validation  Email Address
 
                         Form(
-                          child: Container(
-                            constraints: BoxConstraints.expand(width: 250.0,height: 56.0),
-                            margin: EdgeInsets.only(left: 50.0,top: 100.0),
-                            child: Card(
-                              elevation: 10,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50.0)
-                              ),
-                              child: Form(
-                                   child: Column(
-                                     children: [
-                                       Padding(
-                                       padding: EdgeInsets.only(left: 10),
-                                       child: TextFormField(
+                          //autovalidateMode: AutovalidateMode.,
 
-                                       keyboardType: TextInputType.emailAddress,
-                                         decoration: InputDecoration(
-                                           hintText: 'Enter Email',
-                                           prefixIcon: Icon(Icons.email,
-                                           color: Colors.orange,
-                                           ),
-                                           border: InputBorder.none
-                                         ),
-                                         onChanged: (String value){
+                          key: _formKey,
+                          child: Column(
+                              children: [
+                          Column(
+                            children: [
+                              Container(
+                              constraints: BoxConstraints.expand(width: 250.0,height: 50.0),
+                              margin: EdgeInsets.only(left: 50.0,top: 100.0),
+                              child: Card(
 
-                                         },
-                                         validator: (value){
-                                         return value!.isEmpty?"Please enter email":null;
-                                         },
+                                  elevation: 10,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50.0)
+                                  ),
+
+                                    child: Padding(
+                                      padding: EdgeInsets.only(left: 10),
+                                      child: TextFormField(
+                                       //enabled: true,
+                                        keyboardType: TextInputType.emailAddress,
+                                        decoration: InputDecoration(
+                                            hintText: 'Enter Email',
+                                            // focusedErrorBorder: OutlineInputBorder(
+                                            //   borderSide:
+                                            //     BorderSide(color: validationEmail ? Colors.red : Colors.red,
+                                            //       width: validationEmail ? 2.0 : 0
+                                            //     ),
+                                            //   borderRadius: BorderRadius.circular(20.0),
+                                            // ),
+
+
+
+
+                                            errorStyle: TextStyle(
+                                              fontSize: 10
+                                            ),
+                                            prefixIcon: Icon(Icons.email,
+                                              color: Colors.orange,
+                                            ),
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide(color: validationEmail?Colors.black:Colors.white)
+                                            )
+                                        ),
+                                        onChanged: (String value){
+
+                                        },
+                                        validator: (value){
+                                          setState(() {
+                                            validationEmail=value!.isEmpty;
+
+                                          });
+                                          return null;
+                                          //value!.isEmpty ?" Please enter email": null
+                                        },
+                                      ),
+                                    ),
                                        ),
-                                     ),
-                                       // SizedBox(
-                                       //   height: 40,
-                                       // ),
-                                       // Padding(
-                                       //   padding: EdgeInsets.only(left: 10),
-                                       //   child: TextFormField(
-                                       //
-                                       //     keyboardType: TextInputType.emailAddress,
-                                       //     decoration: InputDecoration(
-                                       //         hintText: 'Enter Email',
-                                       //         prefixIcon: Icon(Icons.email,
-                                       //           color: Colors.orange,
-                                       //         ),
-                                       //         border: InputBorder.none
-                                       //     ),
-                                       //     onChanged: (String value){
-                                       //
-                                       //     },
-                                       //     validator: (value){
-                                       //       return value!.isEmpty?"Please enter email":null;
-                                       //     },
-                                       //   ),
-                                       // ),
-                                   ]),
+                                    ),
+
+                              Padding(
+                                padding: EdgeInsets.only(right: 30.0),
+                                child: Text(
+                                      validationEmail ?" Enter valid email. address": "",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(fontSize: 12,
+                                    color: Colors.red,
+                                    //decoration: TextDecoration.
+                                  ),
+                                  textDirection: TextDirection.ltr,
+
                                 ),
+                              )
+                            ],
+                          ),
+                                Column(
+                                  children:[ Container(
+                                    constraints: BoxConstraints.expand(width: 250.0,height: 50.0),
+                                    margin: EdgeInsets.only(left: 50.0,top: 17.0),
+                                    child: Card(
+                                      elevation: 10,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(50.0)
+                                      ),
+                                      child: Padding(
+                                        padding: EdgeInsets.only(left: 10),
+                                        child: TextFormField(
+
+                                          keyboardType: TextInputType.emailAddress,
+                                          decoration: InputDecoration(
+                                              hintText: 'Enter Password',
+                                              prefixIcon: Icon(Icons.lock,
+                                                color: Colors.orange,
+                                              ),
+                                              border: InputBorder.none
+                                          ),
+                                          onChanged: (String value){
+
+                                          },
+                                          validator: (value){
+                                           setState(() {
+                                             validationPassword=value!.isEmpty;
+                                               });
+                                              return null;
+                                           //value!.isEmpty ?" Please enter email": null
+
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                    Padding(
+                                      padding: EdgeInsets.only(right: 30.0),
+                                      child: Text(
+
+                                        validationPassword ?" Enter valid password": "",
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(fontSize: 12,
+                                          color: Colors.red,
+
+                                          //decoration: TextDecoration.
+                                        ),
+                                        textDirection: TextDirection.ltr,
+
+                                      ),
+                                    )
+                               ] ),
+                             Container(
+                               //constraints: BoxConstraints.expand(width: 250.0,height: 56.0),
+                               margin: EdgeInsets.only(right: 40, top: 1.0),
+                               child: TextButton(
+                                 onPressed: (){},
+                                   child: Text('Forgot Password ?',
+                                   style: TextStyle(
+                                     color: Colors.black,
+                                     fontWeight: FontWeight.w400
+                                   ),
+                                   ),
+                               ),
+                             ),
+
+                                //this container is used to give button for login
+                                Container(
+                                  constraints: BoxConstraints.expand(width: 120.0,height: 50.0),
+                                  margin: EdgeInsets.only(left: 50.0,top: 25.0),
+                                    child: ElevatedButton(
+                                        onPressed:(){
+
+
+                                            if(_formKey.currentState!.validate())
+                                            {
+                                              print("ok");
+                                            }
+
+                                        },
+
+                                      style: ElevatedButton.styleFrom(
+                                        padding: EdgeInsets.all(1.0),
+                                        backgroundColor: Colors.white,
+                                        elevation: 10,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(50.0),
+                                        )
+                                        ),
+                                        child: Ink(
+                                          decoration: BoxDecoration(
+                                              gradient:
+                                              LinearGradient(
+                                                  begin: Alignment.topCenter,
+                                                  end: Alignment.bottomCenter,
+                                                  colors: [Color(0xff58669A), Color(0xff202538)]),
+                                              borderRadius: BorderRadius.circular(50)),
+                                          child: Container(
+                                            width: 121,
+                                            height: 47,
+                                            alignment: Alignment.center,
+                                            child: Text('LOGIN',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.w300,
+                                              ),
+                                            ),
+                                          ),
+                                      ),
+                                    ),
+                                  ),
+
+                             ]
                             ),
                           ),
-                          ),
+                        Container(
+                            margin: EdgeInsets.only(left: 50.0,top: 400.0),
+                            child: RichText(
+                              text: TextSpan(
+                                  text: 'New User? Click',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    fontSize: 15.0
+                                  ),
+                                children: [
+                                  TextSpan(
+                                    text: ' Here',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap=(){print('click');}
+                                  ),
+                                TextSpan(
+                                text: ' To Create Account',
+                                )
+                                ]
+                              ),
+                            )
+                        )
                       ],
                     ),
-                  )
+                  ),
+
               ]
               ),
           ),
